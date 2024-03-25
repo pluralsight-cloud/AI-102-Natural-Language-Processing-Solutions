8e1ce1cd7b8592bdc36b4107a115eae0d4744b86
mulasien <matthew-ulasien@pluralsight.com>
2024-03-25 10:29:27 -0500

    initial commit

---
+++ demos/text_analytics/api-demo/key_phrase_extraction.sh
@@ -0,0 +1,21 @@
+curl -i -X POST "${LANGUAGE_SERVICE_ENDPOINT}language/:analyze-text?api-version=2022-05-01" \
+-H "Ocp-Apim-Subscription-Key: ${LANGUAGE_SERVICE_KEY}" \
+-H "Content-Type: application/json" \
+-d \
+'
+{
+    "kind": "KeyPhraseExtraction",
+    "parameters": {
+        "modelVersion": "latest"
+    },
+    "analysisInput":{
+        "documents":[
+            {
+                "id":"1",
+                "language":"en",
+                "text": "Harrys Bar and Grill in Kansas City has some of the best wings in town. However the wait times can be a bit long, which is disppointing. The decor is unique, featuring statues of mythological figures like the Greek goddess Venus. You can call Harrys Bar and Grill at 456-654-8888."
+            }
+        ]
+    }
+}
+'
